a3de4e9cf2a69ee031acc5b13f8493a0f1a5812c
Nguyễn Thành Đạt <nguyenthanhdat102.hn@gmail.com>
2024-04-27 17:09:31 +0700

    responsive and fix lightbox

--- assets/js/datdev.js
+++ assets/js/datdev.js
@@ -15,9 +15,9 @@ const data = [
     title: "Sapa",
     thumbnail_src: "./assets/images/sapa.png",
     gallery: [
-      "./assets/images/nhatrang.png",
-      "./assets/images/mientay.png",
       "./assets/images/sapa.png",
+      "./assets/images/mientay.png",
+      "./assets/images/halong.png",
     ],
   },
   {
@@ -25,7 +25,7 @@ const data = [
     title: "Nha Trang",
     thumbnail_src: "./assets/images/nhatrang.png",
     gallery: [
-      "./assets/images/nhatrang.png",
+      "./assets/images/mientay.png",
       "./assets/images/mientay.png",
       "./assets/images/sapa.png",
       "./assets/images/mientay.png",
@@ -36,7 +36,7 @@ const data = [
     title: "Miền Tây",
     thumbnail_src: "./assets/images/mientay.png",
     gallery: [
-      "./assets/images/nhatrang.png",
+      "./assets/images/halong.png",
       "./assets/images/mientay.png",
       "./assets/images/sapa.png",
       "./assets/images/mientay.png",
@@ -61,117 +61,118 @@ vid.addEventListener("click", (event) => {
 });
 
 // Item Gallery
-function itemGallery(id, urlThumb, title) {
-  return `<div class="item-gallery relative w-[240px] h-[240px] rounded-3xl rectangle relative shrink-0 cursor-pointer overflow-hidden border-2 border-[#003A34] hover:border-[#D2A97D] hover:text-[#D2A97D]"
-   style="background-image: url(${urlThumb});" onclick="openLightBox(${id})">
+function itemGallery(item) {
+  return `<div class="item-gallery bg-[url(${item.thumbnail_src})] bg-cover relative w-[160px] md:w-[240px] aspect-square rounded-3xl relative shrink-0 cursor-pointer overflow-hidden border-2 border-[#003A34] hover:border-[#D2A97D] hover:text-[#D2A97D]" onclick="openLightBox(${item.id})">
    <div id="title-gallery"
-     class="w-full py-4 gap-2.5 absolute bottom-0 bg-[#FFFFFF] rounded-b-3xl text-center text-xl font-semibold leading-7 hover:underline cursor-pointer ">
-     ${title}
+     class="w-full md:py-4 py-2 gap-2.5 absolute bottom-0 bg-[#FFFFFF] rounded-b-3xl text-center text-sm md:text-xl font-semibold leading-6 md:leading-7 hover:underline cursor-pointer ">
+     ${item.title}
    </div>
  </div>`;
 }
 
 //Render body gallery
 function RenderGalleryString(data) {
-  let galleryContent = "";
-  for (i = 0; i < data.length; i++) {
-    galleryContent += itemGallery(
-      data[i].id,
-      data[i].thumbnail_src,
-      data[i].title
-    );
-  }
-  return galleryContent.toString();
+  const galleryContent = data.map((item) => {
+    return itemGallery(item);
+  });
+  return galleryContent.join("").toString();
 }
 
 // Render HTML
-async function RenderGallery(data) {
+function RenderGallery(data) {
   document.getElementById("gallery").innerHTML = RenderGalleryString(data);
 }
 
 RenderGallery(data);
 
-// Light Box Modal
-function lightBox(title, listImage) {
-  let currentSlide = 1;
+// Build lightbox
+async function lightBox(item) {
+  document.getElementById(
+    "title-lightbox"
+  ).textContent = `Hình ảnh ${item.title}`;
 
-  function next(n) {}
+  //Init current index slide
+  let currentIndex = 0;
 
-  function pre(n) {}
+  const smImgContain = item.gallery.map((img, index) => {
+    return `<img loading="lazy" srcset="${img}" data-index=${index} class="smImg ${
+      index === currentIndex ? "selected" : ""
+    } shrink-0 p-[2px] md:p-1 max-w-24 md:max-w-40 aspect-video cursor-pointer rounded-lg object-cover" />`;
+  });
+  document.getElementById("smImgContain").innerHTML = await smImgContain
+    .join("")
+    .toString(); //wait render img small
 
-  let listImageString = "";
-  if (listImage) {
-    listImageString = listImage.map((item, index) => {
-      if (++index === 1) {
-        return `<img loading="lazy" srcset=${item} class="smImg active shrink-0 w-40 max-w-full aspect-[1.79]"/>`;
-      }
-      return `<img loading="lazy" srcset=${item} class="smImg shrink-0 w-40 max-w-full aspect-[1.79]"/>`;
-    });
+  //Change image when slide change
+  function setImage(img) {
+    document.getElementById("preview").srcset = img.srcset;
   }
 
-  
-  return `
-  <div id="light-box-modal" class="fixed w-full h-screen bg-black top-0 left-0 z-50">
-    <div class="flex flex-col p-6 bg-white rounded-2xl shadow-2xl max-w-[976px] max-md:px-5 fixed top-[50%] left-[50%]  translate-x-[-50%] translate-y-[-50%]">
-      <div class="flex gap-5 justify-between text-2xl font-bold leading-8 text-neutral-800 max-md:flex-wrap max-md:max-w-full">
-        <div class="max-md:max-w-full">${title}</div>
-        <img
-          loading="lazy"
-          src="./assets/icons/close-line.svg"
-          onclick="closeLightBox()"
-          class="shrink-0 w-8 aspect-square cursor-pointer"
-        />
-      </div>
-      <div class="relative">
-        <img
-          loading="lazy"
-          srcset="./assets/images/halong1.jfif"
-          class="mt-6 w-full aspect-[1.79] rounded-2xl max-md:max-w-full"
-        />
-      </div>
-     
-      <div class="flex gap-4 mt-6  max-md:flex-wrap" style="overflow-x: auto;">
-      ${listImageString.join("").toString()}
-      </div>
-    </div>
-  </div>;
-  `;
+  //Set current index slide image
+  const smImgs = document.querySelectorAll(".smImg");
+  setImage(smImgs[currentIndex]);
+
+  smImgs.forEach((img) =>
+    img.addEventListener("click", (e) => {
+      smImgs.forEach((img) => img.classList.remove("selected"));
+      e.target.classList.add("selected");
+      setImage(e.target);
+      currentIndex = e.target.getAttribute("data-index"); //New index when you click slide
+    })
+  );
+
+  const prev = document.getElementById("prev");
+  const next = document.getElementById("next");
+
+  function pushSlide(n) {
+    smImgs[currentIndex].classList.remove("selected");
+    currentIndex += n;
+    if (currentIndex < 0) {
+      currentIndex = smImgs.length - 1;
+    } else if (currentIndex > smImgs.length - 1) {
+      currentIndex = 0;
+    }
+    console.log(currentIndex);
+    smImgs[currentIndex].classList.add("selected");
+    setImage(smImgs[currentIndex]);
+  }
+
+  prev.addEventListener("click", (e) => pushSlide(-1));
+  next.addEventListener("click", (e) => pushSlide(1));
+
+  document.getElementById("wrapper-lightbox").classList.remove("hidden");
 }
 
 // show/hide lightbox
 function openLightBox(id) {
-  data.forEach(async function (item) {
+  data.forEach((item) => {
     if (item.id === id) {
-      const renderHTML = await lightBox(item.title, item.gallery);
-      document.getElementById("wrapper-lightbox").innerHTML = renderHTML;
+      lightBox(item);
+      document.body.style.overflow = 'hidden';
     }
   });
 }
 
 // show/hide lightbox
 function closeLightBox() {
-  document.getElementById("wrapper-lightbox").innerHTML = "";
+  document.getElementById("wrapper-lightbox").classList.add("hidden");
+  document.body.style.overflow = 'auto';
 }
 
 //SHOW OPTION
-function showOption(option = 1) {
-  const option1 = document.getElementById("option-1");
-  const selectOption1 = document.getElementById("option-selected-1");
-  const option2 = document.getElementById("option-2");
-  const selectOption2 = document.getElementById("option-selected-2");
-  if (option !== 1) {
-    option2.className =
-      "px-6 py-3 text-white bg-orange-300 rounded-[100px] max-md:px-5 max-md:max-w-full cursor-pointer";
-    option1.className =
-      "my-auto py-3 px-6 text-zinc-600 max-md:max-w-full cursor-pointer";
-    selectOption2.classList.remove("hidden");
-    selectOption1.classList.add("hidden");
-  } else {
-    option1.className =
-      "px-6 py-3 text-white bg-orange-300 rounded-[100px] max-md:px-5 max-md:max-w-full cursor-pointer";
-    option2.className =
-      "my-auto py-3 px-6 text-zinc-600 max-md:max-w-full cursor-pointer ";
-    selectOption1.classList.remove("hidden");
-    selectOption2.classList.add("hidden");
-  }
-}
+const options = document.querySelectorAll(".option");
+const selectOptions = document.querySelectorAll(".select-option");
+options.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    let index = e.target.getAttribute("index-tab");
+    options.forEach(
+      (tab) =>
+        (tab.className =
+          "option md:px-6 md:py-3 px-4 py-2 text-center md:text-start text-[#5E5E5E] cursor-pointer")
+    );
+    e.target.className =
+      "option md:px-6 md:py-3 px-4 py-2 text-center md:text-start text-white bg-[#D2A97D] rounded-[100px] cursor-pointer";
+    selectOptions.forEach((option) => option.classList.add("hidden"));
+    selectOptions[index - 1].classList.remove("hidden");
+  });
+});
