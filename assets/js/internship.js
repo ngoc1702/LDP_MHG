2de8332866745ce9d6c123ce00e5ddf56905f827
thanhcongnguyen02002 <93530115+thanhcongnguyen02002@users.noreply.github.com>
2024-05-02 09:10:46 +0700

    update

--- assets/js/internship.js
+++ assets/js/internship.js
@@ -59,16 +59,7 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 //   
-var swiper = new Swiper(".mySwiper1", {
-	pagination: {
-	  el: ".swiper-pagination",
-	  type: "progressbar",
-	  clickable: true,
-	  renderBullet: function (index, className) {
-		return '<span class="' + className + '">' + (index + 1) + "</span>";
-	  },
-	},
-  });
+
   var clickicon = document.querySelectorAll('.clickicon');
   //var internIcons = document.querySelectorAll('.intern-icon'); // Lấy tất cả các biểu tượng SVG
   
